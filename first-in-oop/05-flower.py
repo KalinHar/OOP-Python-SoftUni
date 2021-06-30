f8c3a53c1dcef03485f872ce22f71279ed4cd459
KalinHar <kalix@abv.bg>
2021-06-30 16:47:30 +0300

    new

--- first-in-oop/05-flower.py
+++ first-in-oop/05-flower.py
@@ -17,9 +17,11 @@ class Flower:
 
 
 flower = Flower("Lilly", 100)
+flower2 = Flower("Tullup", 100)
 flower.water(50)
 print(flower.status())
 flower.water(60)
 print(flower.status())
 flower.water(100)
 print(flower.status())
+print(flower2.status())
