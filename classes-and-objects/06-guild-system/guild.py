9de8b94069f5a8db38dc918894685af300a04ee9
KalinHar <kalix@abv.bg>
2021-07-03 13:46:56 +0300

    new

---
+++ classes-and-objects/06-guild-system/guild.py
@@ -0,0 +1,35 @@
+from project.player import Player
+
+
+class Guild:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player):
+        if player in self.players:
+            return f"Player {player.name} is already in the guild."
+        if player.guild != "Unaffiliated":
+            return f"Player {player.name} is in another guild."
+        self.players.append(player)
+        player.guild = self.name
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name):
+        for pl in self.players:
+            if pl.name == player_name:
+                pl.guild = "Unaffiliated"
+                self.players.remove(pl)
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        return f"Guild: {self.name}\n" + "\n".join([pl.player_info() for pl in self.players])
+
+
+# player = Player("George", 50, 100)
+# print(player.add_skill("Shield Break", 20))
+# print(player.player_info())
+# guild = Guild("UGT")
+# print(guild.assign_player(player))
+# print(guild.guild_info())
