b62fde91e9efec3553a7156b5aba87b229c5d330
KalinHar <kalix@abv.bg>
2021-07-03 10:39:26 +0300

    new

---
+++ classes-and-objects/05-to-do-list/section.py
@@ -0,0 +1,43 @@
+from project.task import Task
+
+
+class Section:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task):
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
+        self.tasks.append(new_task)
+        return f"Task {new_task.details()} is added to the section"
+
+    def complete_task(self, task_name):
+        for t in self.tasks:
+            if task_name == t.name:
+                t.completed = True
+                return f"Completed task {task_name}"
+        return f"Could not find task with the name {task_name}"
+
+    def clean_section(self):
+        true_task_indexes = [i for i in range(len(self.tasks)) if self.tasks[i].completed]
+        for i in reversed(true_task_indexes):
+            self.tasks.pop(i)
+        return f"Cleared {len(true_task_indexes)} tasks."
+
+    def view_section(self):
+        return f"Section {self.name}:\n" + "\n".join([t.details() for t in self.tasks])
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
