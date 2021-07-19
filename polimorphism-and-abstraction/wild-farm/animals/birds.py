5e9a1415b684d43453a0db2309db944399ab5c7e
KalinHar <kalix@abv.bg>
2021-07-19 09:38:05 +0300

    new

---
+++ polimorphism-and-abstraction/wild-farm/animals/birds.py
@@ -0,0 +1,31 @@
+from project.animals.animal import Bird
+from project.food import Vegetable, Fruit, Meat, Seed
+
+
+class Owl(Bird):
+    _increase_with = 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+    def feed(self, food):
+        if type(food) == Meat:
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Hen(Bird):
+    _increase_with = 0.35
+
+    def make_sound(self):
+        return "Cluck"
+
+    def feed(self, food):
+        if type(food) in (Vegetable, Fruit, Meat, Seed):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
---
+++ polimorphism-and-abstraction/wild-farm/animals/mammals.py
@@ -0,0 +1,58 @@
+from project.animals.animal import Mammal
+from project.food import Vegetable, Fruit, Meat, Seed
+
+
+class Mouse(Mammal):
+    _increase_with = 0.10
+
+    def make_sound(self):
+        return "Squeak"
+
+    def feed(self, food):
+        if type(food) in (Vegetable, Fruit):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Dog(Mammal):
+    _increase_with = 0.40
+
+    def make_sound(self):
+        return "Woof!"
+
+    def feed(self, food):
+        if type(food) == Meat:
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Cat(Mammal):
+    _increase_with = 0.30
+
+    def make_sound(self):
+        return "Meow"
+
+    def feed(self, food):
+        if type(food) in (Vegetable, Meat):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Tiger(Mammal):
+    _increase_with = 1.00
+
+    def make_sound(self):
+        return "ROAR!!!"
+
+    def feed(self, food):
+        if type(food) == Meat:
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * self._increase_with
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
